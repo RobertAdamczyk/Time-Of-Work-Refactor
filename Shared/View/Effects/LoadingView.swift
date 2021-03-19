d9574d9a2214e784e36c0b5b3c909ac422c076d7
You <dzalolx@gmail.com>
2021-03-19 21:00:41 +0100

    Loading View code optimization.

--- Shared/View/Effects/LoadingView.swift
+++ Shared/View/Effects/LoadingView.swift
@@ -10,47 +10,32 @@ import SwiftUI
 import SwiftUI
 
 struct LoadingView: View {
-    @State var startAnimation = false
-    @State var startAnimation2 = false
-    @State var startAnimation3 = false
-    
+    @State var startAnimation: [Bool] = [false, false, false]
+    @State var offset: [CGFloat] = [1, 0 , -1]
     var body: some View {
         GeometryReader { reader in
             ZStack{
                 Color.clear
-                Circle()
-                    .frame(width: reader.size.width/10, height: reader.size.height/10)
-                    .foregroundColor(Color("Orange"))
-                    .offset(x: reader.size.width/4 * 1, y: reader.size.height/2 * -1)
-                    .rotationEffect(.init(degrees: startAnimation ? 0 : -360))
-                Circle()
-                    .frame(width: reader.size.width/10, height: reader.size.height/10)
-                    .foregroundColor(Color("Orange"))
-                    .offset(y: reader.size.height/2 * -1)
-                    .rotationEffect(.init(degrees: startAnimation2 ? 0 : -360))
-                Circle()
-                    .frame(width: reader.size.width/10, height: reader.size.height/10)
-                    .foregroundColor(Color("Orange"))
-                    .offset(x: reader.size.width/4 * -1,y: reader.size.height/2 * -1)
-                    .rotationEffect(.init(degrees: startAnimation3 ? 0 : -360))
+                ForEach(0..<3) { i in
+                    Circle()
+                        .frame(width: reader.size.width/10, height: reader.size.height/10)
+                        .foregroundColor(Color("Orange"))
+                        .offset(x: reader.size.width/4 * offset[i], y: reader.size.height/2 * -1)
+                        .rotationEffect(.init(degrees: startAnimation[i] ? 0 : -360))
+                }
             }
             .frame(width: reader.size.width, height: reader.size.height)
             .onAppear(){
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    withAnimation(Animation.spring(response: 1, dampingFraction: 1, blendDuration: 0).speed(0.6).repeatForever(autoreverses: false)){
-                        startAnimation = true
+                let delay: Double = 0.5
+                let nextDelay: Double = 0.3
+                for i in startAnimation.indices {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + delay + Double(i) * nextDelay) {
+                        withAnimation(Animation.spring(response: 1, dampingFraction: 1, blendDuration: 0).speed(0.6).repeatForever(autoreverses: false)){
+                            startAnimation[i] = true
+                        }
                     }
                 }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
-                    withAnimation(Animation.spring(response: 1, dampingFraction: 1, blendDuration: 0).speed(0.6).repeatForever(autoreverses: false)){
-                        startAnimation2 = true
-                    }
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.1) {
-                    withAnimation(Animation.spring(response: 1, dampingFraction: 1, blendDuration: 0).speed(0.6).repeatForever(autoreverses: false)){
-                        startAnimation3 = true
-                    }
-                }
+               
             }
         }
         
